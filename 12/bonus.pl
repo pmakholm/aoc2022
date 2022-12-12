f2c324ace0a64c4cef439971e317bc41e9169617
Peter Makholm <peter@makholm.net>
2022-12-12 11:33:15 +0100

    Day 12: Solution using priority queues

---
+++ 12/bonus.pl
@@ -0,0 +1,154 @@
+#!/usr/bin/perl
+
+use v5.34;
+use experimental 'signatures';
+
+# This bonus solution actually uses a priority queue. For the given
+# input, this gives a factor 10 speed-up. But I think this changes
+# the solution from something like O(n^2 log n) to O(n log n)
+
+use List::MoreUtils qw(first_index);
+
+my $INT_MAX = 99999;
+
+########################################################################
+#
+# Simple priority queue
+#
+########################################################################
+
+sub Q::new($class) {
+    return bless {}, $class;
+}
+
+sub Q::enqueue($this, $item) {
+    push @{ $this->{ $item->{steps} } }, $item;
+}
+
+sub Q::update($this, $item, $new) {
+    my $old = $item->{steps};
+
+    my $index = first_index { $_ == $item } @{ $this->{$old} };
+    splice @{ $this->{$old} }, $index, 1;
+
+    $item->{steps} = $new;
+
+    push @{ $this->{$new} }, $item;
+}
+
+sub Q::dequeue($this) {
+    # Pop from priority queue
+    my $priority = 0;
+    while (1) {
+        last if $priority > $INT_MAX;
+        unless (exists $this->{$priority}) {
+            $priority++;
+            next;
+        }
+        
+        my $item = shift @{ $this->{$priority} };
+        delete $this->{$priority} if scalar(@{ $this->{$priority} }) == 0;
+        return $item;
+    }
+}
+
+########################################################################
+#
+# Input parsing
+#
+########################################################################
+
+sub new($x,$y,$mark, $start) {
+    my $heigth = ord($mark);
+
+    # Special casing start and end
+    $heigth = ord('a') if $mark eq 'S';
+    $heigth = ord('z') if $mark eq 'E';
+    
+    return {
+        pos => [ $x, $y ],
+        neighbors => [],
+        heigth => $heigth,
+        steps => $mark eq $start ? 0 : $INT_MAX,
+        end => $mark eq 'E',
+    }
+}
+
+sub parse($start, @input) {
+    my $queue = Q->new();
+
+    my @grid;
+    my $x = 0;
+
+    for (@input) {
+        my $y = 0;
+        my @line = map { new($x, $y++, $_, $start) } split //;
+
+        push @grid, \@line;
+        $x++;
+    }
+
+    # Create edges for each possible step, and add it to priority queue
+    my $max_x = $#grid;
+    my $max_y = $#{ $grid[0] };
+    for my $item (map {@$_ } @grid) {
+        my $x = $item->{pos}[0];
+        my $y = $item->{pos}[1];
+
+        for my $pos ( [ $x-1, $y], [$x+1,$y], [$x,$y-1], [$x, $y+1] ) {
+            # Out of bounds
+            next if $pos->[0] < 0
+            || $pos->[0] > $max_x
+            || $pos->[1] < 0
+            || $pos->[1] > $max_y;
+
+            my $neighbor = $grid[ $pos->[0] ][ $pos->[1] ];
+
+            next if $neighbor->{heigth} > $item->{heigth} + 1;
+
+            push @{ $item->{neighbors} }, $neighbor;
+        }
+
+        $queue->enqueue($item);
+    }
+
+    return $queue;
+}
+
+
+########################################################################
+#
+# Do the Dijkstra dance...
+#
+########################################################################
+
+sub dijkstra($queue) {
+    my $item;
+    while (1) {
+        $item = $queue->dequeue();
+        last if $item->{end};
+
+        $item->{visited} = 1;
+
+        for my $neighbor (@{ $item->{neighbors} }) {
+            next if $neighbor->{visited};
+            next if $neighbor->{steps} <= $item->{steps};
+
+            $queue->update($neighbor, $item->{steps} + 1);
+        }
+    }
+
+    return $item;
+}
+
+########################################################################
+#
+# And finally doing each task
+#
+########################################################################
+
+my @input = <>;
+chomp for @input;
+
+say "Task 1: " . dijkstra(parse('S', @input))->{steps} . " steps";
+say "Task 2: " . dijkstra(parse('a', @input))->{steps} . " steps";
