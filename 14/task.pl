7519028f943dd6f8c845f8430dcb6216aef5b785
Peter Makholm <peter@makholm.net>
2022-12-14 15:18:51 +0100

    Trace each grain of sand, and backtrack previos grains

--- 14/task.pl
+++ 14/task.pl
@@ -5,6 +5,14 @@ use experimental 'signatures';
 
 use List::Util qw(max min);
 
+# An obvious optimization over the naive "simulate each grain of sand" is to
+# record the path of each grain and then backtrack the path of the previous
+# grains.
+#
+# In my original submission, I didn't make this optimization. But it only 
+# requires a few lines of code and improves the performance with a factor
+# of 10.
+
 sub add_segment($start, $end, $grid) {
     for my $x (min($start->[0], $end->[0]) .. max($start->[0], $end->[0])) {
         for my $y (min($start->[1], $end->[1]) .. max($start->[1], $end->[1])) {
@@ -41,13 +49,22 @@ sub draw_grid($grid, $depth) {
     }
 }
 
-sub add_sand($grid, $depth, $floor=0) {
-    my ($x, $y) = (500, 0);
+sub add_sand($grid, $depth, $path, $floor=0) {
+    my ($x, $y);
+
+    if (@$path > 2) {
+        pop @$path;
+        ($x, $y) = @{ pop @$path };
+    } else {
+        ($x, $y) = (500, 0);
+    }
 
     # No space for any sand;
     return 0 if $grid->{500}{0};
 
     while(1) {
+        push @$path, [ $x, $y ];
+
         if ($floor) {
             if ($y == $depth + 1) {
                 $grid->{$x}{$y} = 2;
@@ -83,8 +100,9 @@ sub add_sand($grid, $depth, $floor=0) {
 my @input = map { s/\s*$//r } <>;
 my ($grid, $depth)= make_grid(@input);
 
+my $path = [];
 my $count = 0;
-while (add_sand($grid, $depth)) {
+while (add_sand($grid, $depth, $path)) {
     $count++;
 }
 
@@ -92,8 +110,9 @@ say "Task 1: $count";
 
 my ($grid, $depth)= make_grid(@input);
 
+my $path = [];
 my $count = 0;
-while (add_sand($grid, $depth, 1)) {
+while (add_sand($grid, $depth, $path, 1)) {
     $count++;
 }
 
